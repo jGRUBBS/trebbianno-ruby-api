e98d72ba9d199ace8ef9e5dcb56504f882f1742d
Justin Grubbs <justin@jgrubbs.net>
2016-09-01 13:13:36 -0400

    fix test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,13 @@
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default, :development)
+require 'rspec'
+require 'webmock/rspec'
 require 'hashie'
 require 'trebbianno'
 
@@ -17,70 +21,31 @@ RSpec.configure do |config|
   config.order = 'random'
 end
 
-def xml_string(type, body)
-  xml  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-  xml += "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://www.w3.org/2005/08/addressing\">"
-  xml += xml_header_string(type)
-  xml += "<s:Body><#{type} xmlns=\"SII\">"
-  xml += xml_user_string
-  xml += body
-  xml += "</#{type}></s:Body></s:Envelope>"
+def read_xml(type)
+  File.read( File.expand_path("spec/fixtures/#{type}.xml") )
 end
 
-def xml_header_string(type)
-  xml  = "<s:Header><a:Action s:mustUnderstand=\"1\">SII/ISIIService/#{type}</a:Action>"
-  xml += "<a:MessageID>urn:uuid:56b55a70-8bbc-471d-94bb-9ca060bcf99f</a:MessageID>"
-  xml += "<a:ReplyTo><a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address></a:ReplyTo>"
-  xml += "<a:To s:mustUnderstand=\"1\">https://www.trebbianno.us/webservices/SIIService.svc</a:To></s:Header>"
+def fixture(type)
+  XmlSimple.xml_in( read_xml(type) )
 end
 
-def xml_user_string
-  xml  = "<user xmlns:b=\"http://schemas.datacontract.org/2004/07/\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\">"
-  xml += "<b:user_name>&lt;![CDATA[the_username]]&gt;</b:user_name><b:user_password>&lt;![CDATA[the_password]]&gt;</b:user_password></user>"
-end
-
-def xml_address_string(type, address)
-  full_name = "#{address[:first_name]} #{address[:last_name]}"
-  xml  = "<b:#{type}_NAME>&lt;![CDATA[#{full_name}]]&gt;</b:#{type}_NAME>"
-  xml += "<b:#{type}_ADDRESS_1>&lt;![CDATA[#{address[:address1]}]]&gt;</b:#{type}_ADDRESS_1>"
-  xml += "<b:#{type}_ADDRESS_2>&lt;![CDATA[#{address[:address2]}]]&gt;</b:#{type}_ADDRESS_2>"
-  xml += "<b:#{type}_ADDRESS_3>&lt;![CDATA[#{address[:address3]}]]&gt;</b:#{type}_ADDRESS_3>"
-  xml += "<b:#{type}_ADDRESS_CITY>#{address[:city]}</b:#{type}_ADDRESS_CITY>"
-  xml += "<b:#{type}_ADDRESS_COUNTRY>#{address[:country]}</b:#{type}_ADDRESS_COUNTRY>"
-  xml += "<b:#{type}_ADDRESS_STATE>#{address[:state]}</b:#{type}_ADDRESS_STATE>"
-  xml += "<b:#{type}_ADDRESS_ZIP>#{address[:zipcode]}</b:#{type}_ADDRESS_ZIP>"
-  xml += "<b:#{type}_TELEPHONE>#{address[:phone]}</b:#{type}_TELEPHONE>"
-end
-
-def xml_line_items_string(order)
-  xml  = "<b:Order_Details>"
-  order[:line_items].each do |line_item|
-    xml += "<b:Order_Detail_New>"
-    xml += "<b:PRICE>#{line_item[:price]}</b:PRICE>"
-    xml += "<b:QUANTITY>#{line_item[:quantity]}</b:QUANTITY>"
-    xml += "<b:SIZE>#{line_item[:size]}</b:SIZE>"
-    xml += "<b:sku>#{line_item[:sku]}</b:sku>"
-    xml += "</b:Order_Detail_New>"
+def test_line_items_string(order)
+  [].tap do |arr| 
+    order[:line_items].each do |hash| 
+      arr << { price: hash[:price], 
+               quantity: hash[:quantity], 
+               sku: hash[:sku], 
+               size: hash[:size]} 
+     end
   end
-  xml += "</b:Order_Details>"
 end
 
-def xml_order_request_string(order)
-  xml  = "<ohn xmlns:b=\"http://schemas.datacontract.org/2004/07/\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\">"
-  xml += "<b:SERVICE>90</b:SERVICE>"
-  xml += "<b:USER1>&lt;![CDATA[http://example.com/R123123123/invoice]]&gt;</b:USER1>"
-  xml += "<b:CARRIER>FEDEX</b:CARRIER>"
-  xml += "<b:CUSTOMER_CODE/><b:DELIVERY_GIFT_WRAP/>"
-  xml += "<b:DELIVERY_MESSAGE>&lt;![CDATA[Happy Birthday!]]&gt;</b:DELIVERY_MESSAGE>"
-  xml += "<b:EMAIL>someone@somehwere.com</b:EMAIL>"
-  xml += "<b:ORDER_ID>R123123123</b:ORDER_ID>"
-  xml += "<b:ORDER_TYPE>OO</b:ORDER_TYPE>"
-  xml += "<b:DELIVERY_FROM i:nil=\"true\"/><b:DELIVERY_TO i:nil=\"true\"/>"
-  xml += "<b:DELIVERY_ID i:nil=\"true\"/><b:FREIGHT_ACCOUNT i:nil=\"true\"/>"
-  xml += xml_address_string("CUSTOMER", order[:billing_address])
-  xml += xml_address_string("DELIVERY", order[:shipping_address])
-  xml += xml_line_items_string(order)
-  xml += "</ohn>"
+def request_headers
+  {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'text/xml', 'User-Agent'=>'Ruby'}
+end
+
+def inventory_request
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<stock>\n</stock>\n"
 end
 
 def order_hash
@@ -93,7 +58,7 @@ def order_hash
     address2:   "2nd Floor",
     address3:   "",
     city:       "New York",
-    state:      "New York",
+    state:      "NY",
     country:    "US",
     zipcode:    "10012",
     phone:      "123-123-1234"
@@ -115,11 +80,23 @@ def order_hash
   email:        "someone@somehwere.com",
   number:       "R123123123",
   type:         "OO",
+  item_discount: 40,
+  shipping_cost: 30,
+  shipping_discount: 0,
+  tax: 20,
+  shipping_method: 'UPS AIR',
+  date: '01-01-2016',
   line_items: [
     {
-      price:    "127.23",
+      price:    100,
       quantity: "1",
       sku:      "123332211",
+      size:     "XS"
+    },
+    {
+      price:    200,
+      quantity: "1",
+      sku:      "123332212",
       size:     "XS"
     }
   ],
--- trebbianno.gemspec
+++ trebbianno.gemspec
@@ -20,8 +20,10 @@ Gem::Specification.new do |spec|
   spec.add_dependency "xml-simple"
 
   spec.add_development_dependency "hashie"
-  spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
-  spec.add_development_dependency "debugger"
+  spec.add_development_dependency "webmock"
+  spec.add_development_dependency "byebug"
+  spec.add_development_dependency "codeclimate-test-reporter"
 end
